62a6deba65ae61d29efee9b4a506c8f6ac13b249
Bulbasaur854 <tairshechter1118@gmail.com>
2025-06-17 14:10:39 +0300

    hook and get response from Gemini

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    # load gemini api key from `.env` using `dotenv` library
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    # create a new instance of Gemini client using the api key
+    client = genai.Client(api_key=api_key)
+
+    #  get a response from Gemini
+    model = "gemini-2.0-flash-001"
+    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(
+        model=model, contents=contents
+    )
+
+    # print response + prompt and response tokens
+    print(f"""{response.text}
+Prompt tokens: {response.usage_metadata.prompt_token_count}
+Response tokens: {response.usage_metadata.candidates_token_count}""")
+
+if __name__ == "__main__":
+    main()
