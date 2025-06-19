aa944feb7b0b352b321c57891ae93a6ceff7abe0
Bulbasaur854 <tairshechter1118@gmail.com>
2025-06-19 15:22:45 +0300

    get file contents

--- tests.py
+++ tests.py
@@ -1,10 +1,14 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content
 
 def main():
-   print(get_files_info("calculator", ".") + "\n") 
-   print(get_files_info("calculator", "pkg") + "\n") 
-   print(get_files_info("calculator", "/bin") + "\n") 
-   print(get_files_info("calculator", "../")) 
+   # print(get_files_info("calculator", ".") + "\n") 
+   # print(get_files_info("calculator", "pkg") + "\n") 
+   # print(get_files_info("calculator", "/bin") + "\n") 
+   # print(get_files_info("calculator", "../")) 
+
+   print(get_file_content("calculator", "main.py") + "\n")
+   print(get_file_content("calculator", "pkg/calculator.py") + "\n")
+   print(get_file_content("calculator", "/bin/cat"))
 
 if __name__ == "__main__":
     main()
